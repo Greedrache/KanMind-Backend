87d6276cd835ee70c69a98f42a3a86fe8e262063
Greedrache <greedrache@gmail.com>
2026-03-04 12:56:37 +0100

    login und registrations link geändert

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('rest_framework.urls')),  # For browsable API login/logout
-    path('api/user/', include('users.urls')),
+    # Nutzerendpunkte direkt unter /api/ (ohne 'user/' Präfix)
+    path('api/', include('users.urls')),
     path('api/', include('mental.urls')),
 ]
