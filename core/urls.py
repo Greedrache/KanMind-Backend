3c35afcf10746ca3069241a368f4864b61b78d18
Greedrache <greedrache@gmail.com>
2026-03-05 10:09:14 +0100

    Projekt von Config zu Core umbenannt

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('rest_framework.urls')),  # For browsable API login/logout
-    # Nutzerendpunkte direkt unter /api/ (ohne 'user/' Präfix)
+    path('api/auth/', include('rest_framework.urls')), 
     path('api/', include('users.urls')),
     path('api/', include('mental.urls')),
 ]
